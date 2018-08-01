94d5f529cbb2b4392a96e0a748538bc184f8a127
daviro05 <daviro05@ucm.es>
2018-08-01 22:30:56 +0200

    monumentos usuario para android

---
+++ monumentos_usuario.php
@@ -0,0 +1,45 @@
+<?php
+
+session_start();
+include "funciones_BD.php";
+$conexion = conecta();
+
+if($_GET['id_usuario']!="")
+{
+		$usuario=$_GET['id_usuario'];
+		$num_lugares = 0;
+		
+		$usuario=mysqli_real_escape_string($conexion,$usuario);
+
+		$sentencia=mysqli_query($conexion,"SELECT * FROM usuarios_lugares WHERE id_usuario='$usuario'");
+
+		$response = array();
+		//$response["correcto"] = false;  
+
+		if($sentencia){
+			while($fila = mysqli_fetch_array($sentencia))
+			{
+				$num_lugares++;
+				$temp = [
+					'id_usuario' => $fila['id_usuario'],
+					'id_lugar' => $fila['id_lugar'],
+					'nombre_lugar' => $fila['nombre_lugar']
+				];
+				array_push($response, $temp);
+			}
+		}
+
+		//array_push($response, ['num_lugares' => $num_lugares]);
+			
+			//$response["correcto"] = true;
+
+			
+
+/* 			$response["id_usuario"] = $fila['id_usuario'];
+			$response["id_lugar"] = $fila['id_lugar'];
+			$response["nombre_lugar"] = $fila['nombre_lugar']; */
+		
+		echo json_encode($response);
+}
+
+?>
