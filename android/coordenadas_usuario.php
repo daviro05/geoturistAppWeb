a38aafebde0d23c680ccbb678461158f9fdb8729
David Rodriguez <david.rmarco@babel.es>
2018-08-07 10:07:03 +0200

    Calculo de distancia entre coordenadas

--- android/coordenadas_usuario.php
+++ android/coordenadas_usuario.php
@@ -1,39 +1,34 @@
 <?php
 
-session_start();
-include "funciones_BD.php";
-$conexion = conecta();
-
-if($_POST['id_usuario']!="")
+if(1)
 {
-		$usuario=$_POST['id_usuario'];
-		$clave=$_POST['password'];
-		
-			if(validar_clave($clave))
-			{
-				$usuario=mysqli_real_escape_string($conexion,$usuario);
-				$clave=md5(mysqli_real_escape_string($conexion,$clave));
+		//$usuario=$_POST['id_usuario'];
+		$lon1 = -3.6921270999999933;
+		$lat1 = 40.4137818;
 
-				$sentencia=mysqli_query($conexion,"SELECT * FROM usuarios WHERE id_usuario='$usuario' AND password ='$clave'");
+		$lon2 = -3.694900200;
+		$lat2 = 40.4073281;
 
-                $response = array();
-                $response["correcto"] = false;  
+		$punto1 = [$lon1, $lat1];
+		$punto2 = [$lon2, $lat2];
 
-				if(mysqli_num_rows($sentencia)==1)
-				{
-                    $fila=mysqli_fetch_array($sentencia);
-                    
-                    $response["correcto"] = true;
-                    $response["id_usuario"] = $fila['id_usuario'];
-                    $response["nombre"] = $fila['nombre'];
-                    $response["apellidos"] = $fila['apellidos'];
-                    $response["email"] = $fila['email'];
-                }
-                
-                echo json_encode($response);
-				//mysqli_close($conexion);
-				
-			}
+		//para kilómetros
+		echo distance($punto1[0], $punto1[1], $punto2[0], $punto2[1], "K") . " Kilómetros";	
 }
+
+
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+ 
+	$theta = $lon1 - $lon2;
+	$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+	$dist = acos($dist);
+	$dist = rad2deg($dist);
+	$miles = $dist * 60 * 1.1515;
+	$unit = strtoupper($unit);
+
+	$final = $miles * 1.609344;
+   
+	return $final;
+  }
 
 ?>
