68d5f8c231320f235a063855b8f06b03da30e8c9
daviro05 <daviro05@ucm.es>
2018-08-20 18:23:52 +0200

    Add lugar usuario y coordendas

---
+++ add_lugar_usuario.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include "funciones_BD.php";
+$conexion = conecta();
+
+if($_GET['id_lugar']!="")
+{
+	$id_lugar = $_GET['id_lugar'];
+    $id_usuario = $_GET['id_usuario'];
+    $nombre_lugar = $_GET['nombre_lugar'];
+
+    add_lugar_usuario($id_usuario, $id_lugar, $nombre_lugar,$conexion);
+
+}
+
+?>
--- coordenadas_usuario.php
+++ coordenadas_usuario.php
@@ -1,29 +1,53 @@
 <?php
+session_start();
+include "funciones_BD.php";
+$conexion = conecta();
+
+if($_GET['longitud']!="" && $_GET['latitud']!="")
+{
+
+	$longitud_gps = $_GET['longitud'];
+	$latitud_gps = $_GET['latitud'];
+	
+	$punto1 = [$longitud_gps, $latitud_gps];
+	
+
+	$response = array();
+
+	$obtener_lugares = mysqli_query($conexion,"SELECT * FROM lugares");
+
+	if($obtener_lugares){
+
+		while($fila = mysqli_fetch_array($obtener_lugares))
+		{
+			$punto2 = [$fila['longitud'], $fila['latitud']];
+
+			$distancia = distance($punto1[0], $punto1[1], $punto2[0], $punto2[1]);
+
+			//echo $distancia." kilometros <br>";
+
+			if($distancia < 1){ // Si la distancia es menor a 1 kilometro se añade ese monumento al array
+				$temp = [
+					'id_lugar' => $fila['id_lugar'],
+					'nombre_lugar' => $fila['nombre']
+				];
+				array_push($response, $temp);
+			}
+		}
+
+		echo json_encode($response);
+	}
+
+}
 
 
-	//$usuario=$_POST['id_usuario'];
-	$lon1 = -3.6921270999999933;
-	$lat1 = 40.4137818;
-
-	$lon2 = -3.694900200;
-	$lat2 = 40.4073281;
-
-	$punto1 = [$lon1, $lat1];
-	$punto2 = [$lon2, $lat2];
-
-	//para kilómetros
-	echo distance($punto1[0], $punto1[1], $punto2[0], $punto2[1], "K") . " Kilómetros";	
-
-
-
-function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+function distance($lat1, $lon1, $lat2, $lon2) {
  
 	$theta = $lon1 - $lon2;
 	$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
 	$dist = acos($dist);
 	$dist = rad2deg($dist);
 	$miles = $dist * 60 * 1.1515;
-	$unit = strtoupper($unit);
 
 	$final = $miles * 1.609344;
    
